5699dc307dc4d89528c9d117043df1bce456289e
Karumoru <calvinjp07@gmail.com>
2026-01-22 13:13:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'macro-v9-final';
+const cacheName = 'macro-v10-final';
 const assets = [
   './',
   './index.html',
@@ -25,3 +25,4 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
